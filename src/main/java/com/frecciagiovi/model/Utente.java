5f4bbe5862513a7ebb7e6b6b57bce7f0b19fc339
buildingmatty <buildingmatty@gmail.com>
2025-10-14 22:35:14 +0200

    Benvenuto Giubba

--- src/main/java/com/frecciagiovi/model/Utente.java
+++ src/main/java/com/frecciagiovi/model/Utente.java
@@ -1,5 +1,77 @@
 package com.frecciagiovi.model;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class Utente {
 
+    private Long id;
+
+    private String nome;
+
+    private String cognome;
+
+    private String email;
+
+    private LocalDateTime dataNascita;
+
+    public Utente(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getDataNascita() {
+        return dataNascita;
+    }
+
+    public void setDataNascita(LocalDateTime dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Utente utente = (Utente) o;
+        return Objects.equals(id, utente.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Utente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", email='" + email + '\'' +
+                ", dataNascita=" + dataNascita +
+                '}';
+    }
 }
